b1f7472ad4f4cf67c98dac2d551b2d974f00e773
jonswain <jonswain123@gmail.com>
2023-12-20 15:45:10 +0000

    Auto create data folders

---
+++ logd_pka_calculator/utils/project_setup.py
@@ -0,0 +1,11 @@
+"""Functions to set up project"""
+
+import os
+from logd_pka_calculator.utils.paths import make_dir_function as mdf
+
+
+def set_up() -> None:
+    """Creates required data directories"""
+    os.makedirs(mdf("data/external")(), exist_ok=True)
+    os.makedirs(mdf("data/interim")(), exist_ok=True)
+    os.makedirs(mdf("data/processed")(), exist_ok=True)
